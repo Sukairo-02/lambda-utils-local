2cd1052cf125577d6fba5861ac4fc5c9ccaa87a2
Sukairo-02 <reka.serzh@yandex.ru>
2023-08-23 14:15:35 +0300

    Fixed invalid response on corrupted JSON in request

--- packages/functions/image-cropper/src/lambda.ts
+++ packages/functions/image-cropper/src/lambda.ts
@@ -5,8 +5,16 @@ import schema from './schema'
 import crop from './crop'
 
 const lambda: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
-	if(!event.body) throw badRequest('Invalid input')
-	const parseResult = schema.safeParse(JSON.parse(event.body))
+	if (!event.body) throw badRequest('Invalid input')
+	let bodyObj: any
+
+	try {
+		bodyObj = JSON.parse(event.body)
+	} catch (e) {
+		throw badRequest("Unable to parse request's JSON body")
+	}
+
+	const parseResult = schema.safeParse(bodyObj)
 	if (!parseResult.success) throw badRequest('Invalid input', parseResult.error.errors)
 
 	const { image, config } = parseResult.data
