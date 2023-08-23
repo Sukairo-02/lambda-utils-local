e67763fb3ba615970606f745a588c84c4f171ff9
KryvMykyta <kryvmykyta@gmail.com>
2023-08-23 16:24:36 +0300

    done formdata input

--- packages/functions/image-cropper/src/lambda.ts
+++ packages/functions/image-cropper/src/lambda.ts
@@ -8,34 +8,34 @@ import crop from './crop'
 import type { APIGatewayProxyHandler } from 'aws-lambda'
 
 const lambda: APIGatewayProxyHandler = async (event) => {
-	if (!event.body) throw badRequest('Invalid input')
-	let bodyObj: any
+  if (!event.body) throw badRequest('Invalid input')
+  let bodyObj: formParser.MultipartRequest
 
-	try {
-		bodyObj = await formParser.parse(event)
-	} catch (e) {
-		throw badRequest('Unable to parse request data. Make sure request is in multipart/form-data format.')
-	}
+  try {
+    bodyObj = await formParser.parse(event)
+  } catch (e) {
+    throw badRequest('Unable to parse request data. Make sure request is in multipart/form-data format.')
+  }
 
-	const parseResult = schema.safeParse(bodyObj)
-	if (!parseResult.success) throw badRequest('Invalid input', parseResult.error.errors)
+  const parseResult = schema.safeParse(bodyObj)
+  if (!parseResult.success) throw badRequest('Invalid input', parseResult.error.errors)
 
-	const {
-		image: [image],
-		config
-	} = parseResult.data
+  const {
+    files: [{ content: image }],
+    config
+  } = parseResult.data
 
-	const processed = await crop(image, config)
+  const processed = await crop(image, config)
 
-	return {
-		statusCode: 200,
-		body: JSON.stringify({
-			image: processed.toString('base64')
-		}),
-		headers: {
-			'Content-Type': 'image'
-		}
-	}
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      image: processed.toString('base64')
+    }),
+    headers: {
+      'Content-Type': 'image'
+    }
+  }
 }
 
 export const handler = handlerV1(lambda)
--- packages/functions/image-cropper/src/schema.ts
+++ packages/functions/image-cropper/src/schema.ts
@@ -2,47 +2,53 @@ import { z } from 'zod'
 import { badRequest } from '@hapi/boom'
 
 const configSchema = z.object({
-	crop: z
-		.object({
-			top: z.number().nonnegative(),
-			left: z.number().nonnegative(),
-			height: z.number().positive(),
-			width: z.number().positive()
-		})
-		.optional(),
+  crop: z
+    .object({
+      top: z.number().nonnegative(),
+      left: z.number().nonnegative(),
+      height: z.number().positive(),
+      width: z.number().positive()
+    })
+    .optional(),
 
-	resize: z
-		.object({
-			height: z.number().positive().optional(),
-			width: z.number().positive().optional()
-		})
-		.optional(),
+  resize: z
+    .object({
+      height: z.number().positive().optional(),
+      width: z.number().positive().optional()
+    })
+    .optional(),
 
-	compress: z
-		.object({
-			quality: z.number().min(1).max(100).optional(),
-			lossless: z.boolean().optional()
-		})
-		.optional(),
+  compress: z
+    .object({
+      quality: z.number().min(1).max(100).optional(),
+      lossless: z.boolean().optional()
+    })
+    .optional(),
 
-	convert: z
-		.object({
-			format: z.union([z.literal('jpeg'), z.literal('png'), z.literal('webp')])
-		})
-		.optional()
+  convert: z
+    .object({
+      format: z.union([z.literal('jpeg'), z.literal('png'), z.literal('webp')])
+    })
+    .optional()
 })
 
 export default z.object({
-	config: z
-		.string()
-		.transform((content) => {
-			try {
-				return JSON.parse(content)
-			} catch (e) {
-				throw badRequest('Unable to parse config: invalid JSON')
-			}
-		})
-		.pipe(configSchema),
+  config: z
+    .string()
+    .transform((content) => {
+      try {
+        return JSON.parse(content)
+      } catch (e) {
+        throw badRequest('Unable to parse config: invalid JSON')
+      }
+    })
+    .pipe(configSchema),
 
-	image: z.array(z.instanceof(Buffer)).length(1, 'Only one file is allowed per request')
+  files: z
+    .array(
+      z.object({
+        content: z.instanceof(Buffer)
+      })
+    )
+    .length(1, 'Only one file is allowed per request')
 })
