f5368c01cf729d8c5a5a92679d59ef1d75caae2a
Sukairo-02 <reka.serzh@yandex.ru>
2023-08-30 14:30:16 +0300

    Added ad & cookie consent popup blocker

--- packages/functions/pdf-generator/src/generatePDF.ts
+++ packages/functions/pdf-generator/src/generatePDF.ts
@@ -1,10 +1,14 @@
 import puppeteer from 'puppeteer'
 import chromium from '@sparticuz/chromium'
 import { badRequest } from '@hapi/boom'
+import { PuppeteerBlocker } from '@cliqz/adblocker-puppeteer'
+import fetch from 'cross-fetch'
 
 import type { GeneratorOptions } from './types'
 
 const chromiumPath = '/tmp/localChromium/chromium/mac-1165945/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
+
+let blocker: PuppeteerBlocker | undefined
 
 export default async (html: URL | Buffer | string, options?: GeneratorOptions) => {
 	const browser = await puppeteer.launch({
@@ -17,9 +21,18 @@ export default async (html: URL | Buffer | string, options?: GeneratorOptions) =
 	const page = await browser.newPage()
 
 	if (html instanceof URL) {
-		await page.goto(html.toString(), { waitUntil: 'networkidle0' })
+		blocker =
+			blocker ??
+			(await PuppeteerBlocker.fromLists(fetch, [
+				'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt',
+				'https://easylist.to/easylist/easylist.txt'
+			]))
+
+		await blocker.enableBlockingInPage(page)
+
+		await page.goto(html.toString(), { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] })
 	} else {
-		await page.setContent(html.toString(), { waitUntil: 'domcontentloaded' })
+		await page.setContent(html.toString(), { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] })
 	}
 
 	await page.emulateMediaType('screen')
