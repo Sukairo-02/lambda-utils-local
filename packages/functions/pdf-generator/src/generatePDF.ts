fd3ff0d22d1f5b0187440b5b51737adb46170b34
Sukairo-02 <reka.serzh@yandex.ru>
2023-09-03 18:50:18 +0300

    Removed IO dump

--- packages/functions/pdf-generator/src/generatePDF.ts
+++ packages/functions/pdf-generator/src/generatePDF.ts
@@ -46,8 +46,7 @@ export default async (html: URL | Buffer | string, pdfOptions?: PDFOptions, brow
 		defaultViewport: chromium.defaultViewport,
 		executablePath: process.env.IS_LOCAL ? chromiumPath : await chromium.executablePath(),
 		headless: chromium.headless,
-		ignoreHTTPSErrors: true,
-		dumpio: true
+		ignoreHTTPSErrors: true
 	})
 
 	const page = await browser.newPage()
