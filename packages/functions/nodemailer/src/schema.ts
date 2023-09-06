d6a899868fdeaf4ffb6d520f667ea606ddbb1de8
Sukairo-02 <reka.serzh@yandex.ru>
2023-09-06 15:48:12 +0300

    Made optional fields optional

--- packages/functions/nodemailer/src/schema.ts
+++ packages/functions/nodemailer/src/schema.ts
@@ -7,8 +7,8 @@ export default z.object({
 	}),
 	mail: z.object({
 		to: z.string().email(),
-		subject: z.string(),
-		text: z.string(),
+		subject: z.string().optional(),
+		text: z.string().optional(),
 		html: z.string().optional()
 	})
 })
